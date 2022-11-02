9d1c83cd6dfefc617adea822f334ed98fe8b7006
Thanos Sgouros <55700831+iiBezirillo@users.noreply.github.com>
2022-11-02 22:03:50 +0200

    Commit 0,3 Passing data
    Added a pokemonBack image
    Added a card prefab
    
    Adjusted PokemonAPI.cs:
    Now the script can pass data to the children of the Card prefab.
    E.g. One of the children is text. With the PokemonAPI script I can take a specific string from the JSON file create a new string to put the info, and finally take that string and connect it with the text object.

--- Tamasenco Project/Assets/Scripts/PokemonAPI.cs
+++ Tamasenco Project/Assets/Scripts/PokemonAPI.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 using System.Collections;
 using SimpleJSON;
+using UnityEngine.UI;
+using TMPro;
 public class PokemonAPI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI itemNameUI;
+    [SerializeField] private TextMeshProUGUI itemTypeUI;
+    [SerializeField] private TextMeshProUGUI itemHpUI;
+    [SerializeField] private TextMeshProUGUI itemRarityUI;
+    [SerializeField] private RawImage itemIconUI;
+    
+
     private const string URL = "https://api.pokemontcg.io/v2/cards";
-    //private const string HOST = "";
+    //private const string URLType = "https://api.pokemontcg.io/v2/types";
     private const string API_KEY = "ffcdaab5-1147-4617-95e2-a37f37254a9a";
 
     public void GenerateRequest()
     {
         StartCoroutine(ProcessRequest(URL));
+        //StartCoroutine(ProcessRequest(URLType));
     }
 
     private IEnumerator ProcessRequest(string uri)
@@ -31,9 +42,35 @@ public class PokemonAPI : MonoBehaviour
 
                 JSONNode itemsData = JSON.Parse(request.downloadHandler.text);
 
-                int randomNum = Random.Range(1, itemsData["items"]["backpack"].Count);
+                int randomNum = Random.Range(1, itemsData["data"].Count);
 
-                Debug.Log("The generated item is: \nName: " + itemsData["name"]["hp"]["rarity"]);
+                //Debug.Log("The generated item is: \nName: " + itemsData["data"][randomNum]["name"]);
+
+                string itemName = itemsData["data"][randomNum]["name"];
+                //List<string> itemType = new List<string>();
+                //itemType = itemsData["data"][randomNum]["evolvesTo"];
+                string itemHp = itemsData["data"][randomNum]["hp"];
+                string itemRarity = itemsData["data"][randomNum]["rarity"];
+                string itemImageUrl = itemsData["data"][randomNum]["images"]["large"];
+
+                UnityWebRequest itemImageRequest = UnityWebRequestTexture.GetTexture(itemImageUrl);
+
+                yield return itemImageRequest.SendWebRequest();
+
+                if (itemImageRequest.isNetworkError || itemImageRequest.isHttpError)
+                {
+                    Debug.LogError(itemImageRequest.error);
+                    yield break;
+                }
+
+                itemNameUI.text = "Name: " + itemName;
+                //itemTypeUI.text = "Type: " + itemType;
+                itemHpUI.text = "HP: " + itemHp;
+                itemRarityUI.text = "Rarity: " + itemRarity;
+                itemIconUI.texture = DownloadHandlerTexture.GetContent(itemImageRequest);
+                itemIconUI.texture.filterMode = FilterMode.Point;
+
+                
             }
         }
     }
