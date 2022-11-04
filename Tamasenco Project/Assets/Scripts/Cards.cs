8fedf53ddd7f787a374973030c240b5fc0660535
Thanos Sgouros <55700831+iiBezirillo@users.noreply.github.com>
2022-11-04 19:23:58 +0200

    Commit 0.4 fetch data for multiple cards
    Added 49 duplicates of the card prefab to have 50 cards in total.
    Added a CardHolder object to keep all the cards in.
    Added a ScrollRect as parent of CardHolder to be able to scroll through the cards.
    Added a Deck gameobject to represnt the deck building system.
    Added some Plugins.
    
    Adjusted button:
    button is located in the Deck gameobject and can be pressed to fetch information to all 50 cards. The values of the cards are completely randomized due to a randomNum integer, which uses random.range to generate random data for the cards.

---
+++ Tamasenco Project/Assets/Scripts/Cards.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+//[System.Serializable]
+//public class Cards
+//{
+
+//    [SerializeField] public TextMeshProUGUI itemNameUI;
+//    //[SerializeField] public TextMeshProUGUI itemTypeUI;
+//    [SerializeField] public TextMeshProUGUI itemHpUI;
+//    [SerializeField] public TextMeshProUGUI itemRarityUI;
+//    [SerializeField] public RawImage itemIconUI;
+
+
+//}
--- Tamasenco Project/Assets/Scripts/PokemonAPI.cs
+++ Tamasenco Project/Assets/Scripts/PokemonAPI.cs
@@ -5,14 +5,19 @@ using System.Collections;
 using SimpleJSON;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+using Newtonsoft.Json;
+
 public class PokemonAPI : MonoBehaviour
 {
+    //public Cards[] card;     
+
     [SerializeField] private TextMeshProUGUI itemNameUI;
-    [SerializeField] private TextMeshProUGUI itemTypeUI;
     [SerializeField] private TextMeshProUGUI itemHpUI;
     [SerializeField] private TextMeshProUGUI itemRarityUI;
+    //[SerializeField] private TextMeshProUGUI itemTypeUI;
     [SerializeField] private RawImage itemIconUI;
-    
+
 
     private const string URL = "https://api.pokemontcg.io/v2/cards";
     //private const string URLType = "https://api.pokemontcg.io/v2/types";
@@ -21,7 +26,6 @@ public class PokemonAPI : MonoBehaviour
     public void GenerateRequest()
     {
         StartCoroutine(ProcessRequest(URL));
-        //StartCoroutine(ProcessRequest(URLType));
     }
 
     private IEnumerator ProcessRequest(string uri)
@@ -46,9 +50,10 @@ public class PokemonAPI : MonoBehaviour
 
                 //Debug.Log("The generated item is: \nName: " + itemsData["data"][randomNum]["name"]);
 
+
+
                 string itemName = itemsData["data"][randomNum]["name"];
-                //List<string> itemType = new List<string>();
-                //itemType = itemsData["data"][randomNum]["evolvesTo"];
+                //string itemType = itemsData["data"][randomNum]["types"];
                 string itemHp = itemsData["data"][randomNum]["hp"];
                 string itemRarity = itemsData["data"][randomNum]["rarity"];
                 string itemImageUrl = itemsData["data"][randomNum]["images"]["large"];
@@ -70,7 +75,20 @@ public class PokemonAPI : MonoBehaviour
                 itemIconUI.texture = DownloadHandlerTexture.GetContent(itemImageRequest);
                 itemIconUI.texture.filterMode = FilterMode.Point;
 
-                
+                //TextMeshProUGUI nameUI = GetComponent<Cards>().itemNameUI;
+                ////TextMeshProUGUI TypeUI = GetComponentInChildren<Cards>().itemNameUI;
+                //TextMeshProUGUI HpUI = GetComponent<Cards>().itemNameUI;
+                //TextMeshProUGUI RarityUI = GetComponent<Cards>().itemNameUI;
+                //RawImage IconUI = GetComponent<Cards>().itemIconUI;
+
+                //nameUI.text = "Name: " + itemName;
+                ////TypeUI.text = "Type: " + itemType;
+                //HpUI.text = "HP: " + itemHp;
+                //RarityUI.text = "Rarity: " + itemRarity;
+                //IconUI.texture = DownloadHandlerTexture.GetContent(itemImageRequest);
+                //IconUI.texture.filterMode = FilterMode.Point;
+
+
             }
         }
     }
